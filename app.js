468bbe396a273422a33b1868111cc6b1b20c8a34
JuDev <jahidudd65@gmail.com>
2021-10-12 20:28:24 +0200

    CSS binding

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 "use strict";
 
+// libraries
 const http = require("http");
-const path = require('path');
-
 const express = require("express");
+
+// express api
 const app = express();
 
-app.use(express.static(path.join(__dirname + '/public/static/css')));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/templates/index.html");
+    res.sendFile(__dirname + "/templates/index.html");
 });
 
+app.get("/gifs/:search", (req, res) => {
+    res.send(req.params);
+});
+
+// server
 const server = http.createServer(app);
 
 server.listen(3000, () => {

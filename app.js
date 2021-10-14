9e5f9f3fe833868918ca84b1c5c3757230a8ceb5
JuDev <jahidudd65@gmail.com>
2021-10-14 11:47:15 +0200

    push

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+// home
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/templates/index.html");
 });
 
+// docs
+app.get("/docs", (req, res) => {
+    res.sendFile(__dirname + "/templates/docs.html");
+});
+
+// about
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/templates/about.html");
+});
+
+// request
 app.get("/gifs/:search", (req, res) => {
     res.send(req.params);
 });

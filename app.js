9a383cae80cb30348783b80c3c04f826071483de
JuDev <jahidudd65@gmail.com>
2021-10-16 12:11:22 +0200

    push

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 "use strict";
 
-// libraries
 const http = require("http");
+
 const express = require("express");
 
-// express api
+const logger = require("./public/javascript/logger")
+
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-// home
+app.use(logger({
+    level: "info"
+}));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/templates/index.html");
 });
 
-// docs
 app.get("/docs", (req, res) => {
     res.sendFile(__dirname + "/templates/docs.html");
 });
 
-// about
 app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/templates/about.html");
 });
 
-// request
 app.get("/gifs/:search", (req, res) => {
     res.send(req.params);
 });
 
-// server
 const server = http.createServer(app);
 
 server.listen(5000, () => {

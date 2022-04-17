3c115c4cb5385f3808a12c97560a3cc9f344203a
JuDev <jahidudd65@gmail.com>
2022-04-17 16:08:10 +0200

    Performance boost + /gifs returns only the gif now

--- public/javascript/fetch.js
+++ public/javascript/fetch.js
@@ -1,41 +1,35 @@
 "use strict";
 
-const puppeteer = require("puppeteer");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 function getRandomNumber(min, max) {
     let step_1 = max - min + 1;
     let step_2 = Math.random() * step_1;
-    let result = Math.floor(step_2) + min;
 
-    return result
+    return Math.floor(step_2) + min;
 }
 
 async function gifs(search) {
     const url = `https://tenor.com/search/${search}`;
 
-    const browser = await puppeteer.launch({
-        'args': [
-            '--no-sandbox'
-        ],
-        headless: true
-    });
+    return (
+        axios.get(url)
+        .then(response => {
+            let $ = cheerio.load(response.data);
+            const listOfGifs = [];
 
-    const page = await browser.newPage();
-    await page.goto(url);
+            $('img').each((_index, element) => {
+                listOfGifs.push($(element).attr("src"));
+            });
 
-    const listOfGifs = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll("img")).map((gifs) => gifs.src);
-    });
+            listOfGifs.splice(0, 2);
 
-    listOfGifs.splice(0, 3);
-
-    const result = getRandomNumber(0, listOfGifs.length - 1);
-
-    browser.close();
-
-    return {
-        "gif": listOfGifs[result]
-    };
+            return listOfGifs[getRandomNumber(0, listOfGifs.length - 1)];
+        }).catch(error => {
+            console.error(error);
+        })
+    )
 }
 
 module.exports = gifs;

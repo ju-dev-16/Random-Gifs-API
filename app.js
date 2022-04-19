6504b8b5fb3617f15623506b198259166c30b325
JuDev <jahidudd65@gmail.com>
2022-04-19 10:12:17 +0200

    little fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const logger = require("./public/javascript/logger");
 const fetch = require("./public/javascript/fetch");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(express.static(__dirname + "/public"));
 
